e984939b226f13b6056f734c215c1374c39dea20
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-09-19 11:04:40 -0700

    test2

---
+++ C# Code Based Test2/CodeTest 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student UgStudent = new Ug("Ashwin", 370118, 74.3);
+            Student GradStudent = new Graduated("Esakki", 360011, 84.9);
+            Console.WriteLine($"{UgStudent.Name}({UgStudent.StudentId}) has Passed the UnderGraduate with an Grade of {UgStudent.Grade} : {UgStudent.IsPassed(UgStudent.Grade)}");
+
+            Console.WriteLine($"{GradStudent.Name}({GradStudent.StudentId}) has Graduated with an Grade of {GradStudent.Grade} : {GradStudent.IsPassed(GradStudent.Grade)}");
+            Console.ReadLine();
+        }
+    }
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+        public Student(string name,int studid, double grade)
+        {
+            Name = name;
+            StudentId = studid;
+            Grade = grade;
+
+        }
+        public abstract bool IsPassed(double grade);
+    }
+    class Ug : Student
+    {
+        public Ug(string name, int studid, double grade) : base(name,studid,grade)
+        {
+
+        }
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+    class Graduated : Student
+    {
+        public Graduated(string name,int studid, double grade):base(name,studid,grade)
+        {
+
+        }
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+
+    }
+}
---
+++ C# Code Based Test2/CodeTest2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            for(int i =1 ; i<=10 ; i++)
+            {
+                Console.WriteLine($"Enter the Details of an Product {i}:");
+                Console.WriteLine("Product Name: ");
+                string prodname = Console.ReadLine();
+                Console.WriteLine("Price of the Product: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                products.Add(new Product(i, prodname, price));
+                Console.ReadLine();
+            }
+            products = products.OrderBy(p => p.PriceOfProduct).ToList();
+            Console.WriteLine("\n" + " Sorted Products by Price: \n");
+            foreach(var product in products)
+            {
+                Console.WriteLine($"Product Id: {product.ProdId} \n Product name: {product.ProdName} \n Product Price: {product.PriceOfProduct}");
+                Console.ReadLine();
+            }
+        }
+    }
+    class Product
+    {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public double PriceOfProduct { get; set; }
+        public Product(int prodid , string prodname, double price)
+        {
+            ProdId = prodid;
+            ProdName = prodname;
+            PriceOfProduct = price;
+        }
+    }
+}
