e6800a5f7ac761f04c6888b1783e4402c828470b
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-09-24 17:05:27 -0700

    Assignment_6

---
+++ Assignments/Assignment_6/Concession/Concession/Concession/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class concession
+    {
+        public int Price = 500;
+        public string Name;
+        public int Age;
+
+        public void CalculatorConsession()
+        {
+            if (Age <= 5)
+            {
+                Console.WriteLine("Your name : {0}  \nYour age : {1} \nYou have won a free ticket Champion , Bravo! ", Name, Age);
+            }
+            else if (Age > 60)
+            {
+                double TicketFair = Price * 0.30;
+                double dprice = Price - TicketFair;
+                Console.WriteLine("Senior citizen TicketFare : {0} \nYour name : {1} \nYour age : {2}", dprice, Name, Age);
+            }
+
+            else
+            {
+                Console.WriteLine("Your tickets are booked {0} \nYour name : {1} \nYour age : {2}", Price, Name, Age);
+            }
+
+        }
+
+    }
+
+}
---
+++ Assignments/Assignment_6/Concession/ConcessionApplication/ConcessionApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concession;
+
+namespace ConsessionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Concession.concession calculator = new Concession.concession
+            {
+                Name = Name,
+                Age = Age
+            };
+            calculator.CalculatorConsession();
+            Console.ReadLine();
+
+
+        }
+    }
+}
