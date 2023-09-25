8d5e2f55c81ac31c761cd4998ea35df8b2d62cb3
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-09-25 13:26:55 -0700

    codeTest3

---
+++ CSharp Code Based Test/C# Code Based Test3/Code_1/Code_1/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_1
+{
+    class Box
+    {
+        public int Len { get; set; }
+        public int Bred { get; set; }
+        public Box(int len,int bred)
+        {
+            Len = len;
+            Bred = bred;
+        }
+        public Box AddSomeBoxes(Box extraBox)
+        {
+            int totalLen = Len + extraBox.Len;
+            int totalBred = Bred + extraBox.Bred;
+            return new Box(totalLen, totalBred);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Length: {Len} units");
+            Console.WriteLine($"Breadth: {Bred} units");
+        }
+    }
+    class BoxProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value of Length:");
+            int lenval = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("enter the value of Breadth:");
+            int bredval = Convert.ToInt32(Console.ReadLine());
+            Box box1 = new Box(lenval, bredval);
+            Console.WriteLine("enter the value of Length for box 2:");
+            lenval = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of Breadth for box 2: ");
+            bredval = Convert.ToInt32(Console.ReadLine());
+            Box box2 = new Box(lenval, bredval);
+            Box box3 = box1.AddSomeBoxes(box2);
+            Console.WriteLine("The details of the Box 3:");
+            box3.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp Code Based Test/C# Code Based Test3/Code_1/Code_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cricket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of matches played:");
+            if(int.TryParse(Console.ReadLine(), out int match_count)&& match_count >0)
+            {
+                Cricket game = new Cricket();
+                game.Points(match_count);    
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input, Check the value which is given as an Input and Enter the valid Input.");
+            }
+            Console.ReadLine();
+        }
+    }
+    class Cricket
+    {
+        public void Points(int match_count)
+        {
+            int[] Scores = new int[match_count];
+            int Sum = 0;
+            for(int i = 0 ; i<match_count ;i++)
+            {
+                Console.WriteLine($"Enter the score{i+1}:");
+                if(int.TryParse(Console.ReadLine(),out int Score))
+                {
+                    Scores[i] = Score;
+                    Sum += Score;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong score, Enter the valid score details");
+                    i--;
+                }
+            }
+            double Avg = Convert.ToDouble(Sum / match_count);
+            Console.WriteLine("Total Score: {0}", Sum);
+            Console.WriteLine("Average Scored: {0}", Avg);
+            Console.ReadLine();
+            
+        }
+    }
+    
+}
