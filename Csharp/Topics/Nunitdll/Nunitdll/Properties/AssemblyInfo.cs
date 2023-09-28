95cb2713c74fc45e13e3c1a58fe3fc255640d84c
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-09-28 16:59:42 -0700

    Sql

---
+++ Csharp/Topics/Nunitdll/Nunitdll/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nunitdll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nunitdll")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7ac7c39-e267-4901-a836-8cb24deab358")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Csharp/Topics/testaccount/testaccount/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testaccount
+{
+    public class Class1
+    {
+        public int Add()
+        {
+            Console.WriteLine("ji");
+            return 1;
+        }
+    }
+}
