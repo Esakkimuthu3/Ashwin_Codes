4c9da1cef075bd40e7567f9091cd72a409a6ac66
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-10-23 11:48:52 -0700

    code6

--- SQL/CodeBasedTest/CodeTest2/Codebased6.sql
+++ SQL/CodeBasedTest/CodeTest2/Codebased6.sql
@@ -9,72 +9,50 @@ CREATE TABLE Code_Employees (
     emptype char(1) CHECK (emptype IN ('F', 'P'))
 );
 
-CREATE PROCEDURE AddEmployees
-    @empno int,
+CREATE PROCEDURE AddEmployeese
+    
     @empname varchar(35),
     @empsal decimal(10,2),
     @emptype char(1)
 AS
 BEGIN
+   declare @empno int;
+    set @empno = (select isnull(max(empno), 0) + 1 from Code_Employees);
     INSERT INTO Code_Employees(empno, empname, empsal, emptype)
     VALUES (@empno, @empname, @empsal, @emptype)
 END;
 --select* from Employee
  
-EXEC AddEmployees 7369,'Smith',8000.00,'F'
-EXEC AddEmployees 7499,'Allen',5001.00,'P'
-EXEC AddEmployees 7521,'Ward',6000.00,'F'
-EXEC AddEmployees 7566,'Jones',2975.00,'P'
+EXEC AddEmployeese 'Smith',8000.00,'F'
+EXEC AddEmployeese 'Allen',5001.00,'P'
+EXEC AddEmployeese 'Ward',6000.00,'F'
+EXEC AddEmployeese 'Jones',2975.00,'P'
 
 --select*from Code_Employees
 
+
+
 --2.Code2
 
--- Declare the variables for the cursor
+
 DECLARE @empnum NUMERIC(4);
 DECLARE @salary INT;
-
- 
-
--- Declare the cursor
 DECLARE employee_cursors CURSOR FOR
 SELECT empno, salary
 FROM employee
 WHERE deptno = 10;
-
- 
-
--- Open the cursor
 OPEN employee_cursors;
-
- 
-
--- Fetch the first row
 FETCH NEXT FROM employee_cursors INTO @empnum, @salary;
-
- 
-
--- Loop through the cursor
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    -- Update the salary with a 15% increase
     UPDATE employee
     SET salary = salary * 1.15
     WHERE empno = @empnum;
-
- 
-
-    -- Fetch the next row
     FETCH NEXT FROM employee_cursors INTO @empnum, @salary;
-END
+END;
 
- 
-
--- Close and deallocate the cursor
 CLOSE employee_cursors;
 DEALLOCATE employee_cursors;
-
---Select the updated value for verification
 SELECT * FROM Employee WHERE deptno = 10;
 
 
