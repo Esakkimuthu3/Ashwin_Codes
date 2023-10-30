5cbba760ff84d51dbf9bd8598443c61e24fd9504
Esakkimuthu Venkatesan <esakkimuthuv@infinite.com>
2023-10-30 11:40:00 -0700

    codetestweb

---
+++ WebTechnology/CodeBasedTest/Question3/Question3/Validator.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Validator.aspx.cs" Inherits="Question3.Validator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+
+        <div>
+            <h3>Insert your Details</h3>
+            <br />
+            <label for="txtName">Name:</label>
+            <asp:TextBox ID="txtName" runat="server" placeholder="Name"></asp:TextBox><br />
+            <br />
+            <label for="txtFamilyName">Family Name:</label>
+            <asp:TextBox ID="txtFamilyName" runat="server" placeholder="Family Name"></asp:TextBox><br />  
+            <br />
+            <label for="txtAddress">Address:</label>
+            <asp:TextBox ID="txtAddress" runat="server" placeholder="Address"></asp:TextBox><br />  
+            <br />
+            <label for="txtCity">City:</label>
+            <asp:TextBox ID="txtCity" runat="server" placeholder="City"></asp:TextBox><br />   
+            <br />
+            <label for="txtZipCode">Zip Code:</label>
+            <asp:TextBox ID="txtZipCode" runat="server" placeholder="Zip Code"></asp:TextBox><br /> 
+            <br />
+            <label for="txtPhone">Phone:</label>
+            <asp:TextBox ID="txtPhone" runat="server" placeholder="Phone"></asp:TextBox><br />  
+            <br />
+            <label for="txtEmail">E-mail:</label>
+            <asp:TextBox ID="txtEmail" runat="server" placeholder="Email" ></asp:TextBox><br /> 
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Check" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebTechnology/CodeBasedTest/Question3/Question3/Validator.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Question3
+{
+    public partial class Validator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //string script = "window.open('http://www.example.com', '_self', 'width=500,height=500')";
+            //script += "?Name=" + txtName.Text;
+            //script += "&FamilyName=" + txtFamilyName.Text;
+            //script += "&Address=" + txtAddress.Text;
+            //script += "&City=" + txtCity.Text;
+            //script += "&ZipCode=" + txtZipCode.Text;
+            //script += "&Phone=" + txtPhone.Text;
+            //script += "&Email=" + txtEmail.Text;
+
+            //ClientScript.RegisterStartupScript(this.GetType(), "openwindow", script, true);
+
+            string name = txtName.Text;
+            string familyName = txtFamilyName.Text;
+            string address = txtAddress.Text;
+            string city = txtCity.Text;
+            string zipCode = txtZipCode.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+
+
+            
+            string resultMessage = $@"Name: {name}<br />
+                                     Family Name: {familyName}<br />
+                                     Address: {address}<br />
+                                     City: {city}<br />
+                                     Zip Code: {zipCode}<br />
+                                     Phone: {phone}<br />
+                                     Email: {email}<br />";
+
+           
+            resultLabel.Text = resultMessage;
+        }
+    }
+
+}
+   
